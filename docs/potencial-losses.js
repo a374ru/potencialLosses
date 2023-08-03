a2e1fc640d26b531384c8c599cfafd2535b98f56
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-03 20:37:32 +0300

    ••• 2023

---
+++ docs/potencial-losses.js
@@ -0,0 +1,18 @@
+// Реальный потенциальный убыток по недопониманию созданный субъектом права
+// Доход был оборван 2023/08/01 и начал расти завуалированный убыток
+setInterval(function () {
+    var SECMONF = 2635200; // среднее потерь в секунду
+    var kavkazDebet = 40000;
+    var grayDebet = 45000;
+    var allDebet = kavkazDebet + grayDebet;
+    var startUbitka = new Date('2023-08-01');
+    var dateUbitka = startUbitka.toLocaleString().slice(0, -10);
+    var start_ubitka = document.querySelector("#start-ubitka");
+    var rrr = start_ubitka.innerHTML = dateUbitka;
+    var curentDate = new Date();
+    var intervalDate = (curentDate.getTime() - startUbitka.getTime()) / 1000;
+    var potencialLosses = allDebet / SECMONF * intervalDate;
+    /** Вставка данных в документ */
+    var potencial_Losses = document.querySelector("#potencial-losses");
+    potencial_Losses.innerHTML = potencialLosses.toFixed(2) + " ₽";
+}, 1000);
---
+++ docs/potencial-losses.ts
@@ -0,0 +1,24 @@
+
+// Реальный потенциальный убыток по недопониманию созданный субъектом права
+// Доход был оборван 2023/08/01 и начал расти завуалированный убыток
+setInterval(function () {
+	const SECMONF = 2635200 // среднее потерь в секунду
+	const kavkazDebet = 40000
+	const grayDebet = 45000
+	const allDebet = kavkazDebet + grayDebet
+	const startUbitka = new Date('2023-08-01')
+	const dateUbitka = startUbitka.toLocaleString().slice(0, -10)
+	const start_ubitka = document.querySelector("#start-ubitka")
+	let rrr = start_ubitka!.innerHTML = dateUbitka
+
+
+	let curentDate = new Date()
+	let intervalDate = (curentDate.getTime() - startUbitka.getTime()) / 1000
+
+	let potencialLosses = allDebet / SECMONF * intervalDate
+	/** Вставка данных в документ */
+
+	const potencial_Losses = document.querySelector("#potencial-losses")
+
+	potencial_Losses!.innerHTML = potencialLosses.toFixed(2) + " ₽"
+}, 1000)
