4367dfdd520b2a08e15efdb0757a1fc5869b80be
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-07 07:59:50 +0300

    ••• 2023

--- docs/potencial-losses.js
+++ docs/potencial-losses.js
@@ -4,16 +4,16 @@ var sizeOfCost = 2;
 if (speedIntervalMLS < 77) {
     sizeOfCost = 3;
 }
+var kavkazDebet = 40000 * 12;
+var grayDebet = 45000 * 12;
+var allDebet = kavkazDebet + grayDebet;
+var theCostOfASecond = allDebet / 365 / 86400;
+var startUbitka = new Date('2023-08-01');
+var dateUbitka = startUbitka.toLocaleDateString();
+var start_ubitka = document.getElementById("start-ubitka");
+start_ubitka.innerHTML = dateUbitka;
 setInterval(function () {
     var curentDate = new Date();
-    var kavkazDebet = 40000;
-    var grayDebet = 45000;
-    var allDebet = kavkazDebet + grayDebet;
-    var theCostOfASecond = allDebet / 30.5 / 86400;
-    var startUbitka = new Date('2023-08-01');
-    var dateUbitka = startUbitka.toLocaleDateString();
-    var start_ubitka = document.getElementById("start-ubitka");
-    start_ubitka.innerHTML = dateUbitka;
     var intervalDate = (curentDate.getTime() - startUbitka.getTime()) / 1000;
     var potencialLosses = theCostOfASecond * intervalDate;
     var potencial_Losses = document.getElementById("potencial-losses");
