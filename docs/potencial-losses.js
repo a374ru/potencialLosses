a819e2a32617ae8ffd02ebbf51acda333f9c1705
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-07 07:48:50 +0300

    ••• 2023

--- docs/potencial-losses.js
+++ docs/potencial-losses.js
@@ -1,18 +1,21 @@
-// Реальный потенциальный убыток по недопониманию созданный субъектом права
-// Доход был оборван 2023/08/01 и начал расти завуалированный убыток
+"use strict";
+var speedIntervalMLS = 1000;
+var sizeOfCost = 2;
+if (speedIntervalMLS < 77) {
+    sizeOfCost = 3;
+}
 setInterval(function () {
     var curentDate = new Date();
-    var SECMONF = 2635200; // среднее потерь в секунду
     var kavkazDebet = 40000;
     var grayDebet = 45000;
     var allDebet = kavkazDebet + grayDebet;
+    var theCostOfASecond = allDebet / 30.5 / 86400;
     var startUbitka = new Date('2023-08-01');
     var dateUbitka = startUbitka.toLocaleDateString();
     var start_ubitka = document.getElementById("start-ubitka");
     start_ubitka.innerHTML = dateUbitka;
     var intervalDate = (curentDate.getTime() - startUbitka.getTime()) / 1000;
-    var potencialLosses = allDebet / SECMONF * intervalDate;
-    /** Вставка данных в документ */
+    var potencialLosses = theCostOfASecond * intervalDate;
     var potencial_Losses = document.getElementById("potencial-losses");
-    potencial_Losses.innerHTML = "₽ " + potencialLosses.toFixed(2);
-}, 1000);
+    potencial_Losses.innerHTML = "₽ " + potencialLosses.toFixed(sizeOfCost);
+}, speedIntervalMLS);
--- docs/potencial-losses.ts
+++ docs/potencial-losses.ts
@@ -1,24 +1,33 @@
 
 // Реальный потенциальный убыток по недопониманию созданный субъектом права
 // Доход был оборван 2023/08/01 и начал расти завуалированный убыток
+let speedIntervalMLS = 1000
+
+let sizeOfCost = 2
+if (speedIntervalMLS < 77) {
+	sizeOfCost = 3
+}
+
 setInterval(function () {
 	let curentDate = new Date()
-	const SECMONF = 2635200 // среднее потерь в секунду
 	const kavkazDebet = 40000
 	const grayDebet = 45000
 	const allDebet = kavkazDebet + grayDebet
-	const startUbitka = new Date('2023-08-01')
+
+
+	// const theCostOfASecond = 2635200 // среднее потерь в сукунду
+	const theCostOfASecond = allDebet / 30.5 / 86400
+
+	const startUbitka = new Date('2023-08-01') // дата прекращения ренты клиенту по решению Михеевой Н.В.
 	const dateUbitka = startUbitka.toLocaleDateString()
 	const start_ubitka = document.getElementById("start-ubitka")
 	start_ubitka!.innerHTML = dateUbitka
 
-
 	let intervalDate = (curentDate.getTime() - startUbitka.getTime()) / 1000
 
-	let potencialLosses = allDebet / SECMONF * intervalDate
-	/** Вставка данных в документ */
+	let potencialLosses = theCostOfASecond * intervalDate
 
 	const potencial_Losses = document.getElementById("potencial-losses")
 
-	potencial_Losses!.innerHTML = "₽ " + potencialLosses.toFixed(2)
-}, 1000)
+	potencial_Losses!.innerHTML = "₽ " + potencialLosses.toFixed(sizeOfCost)
+}, speedIntervalMLS)
